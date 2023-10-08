cb7d2d7e937ade02cdb87ae03e1711901bfa7e12
Pascal Lalonde <plalonde@overnet.ca>
2023-10-07 23:39:09 -0400

    Verification working with CRLs, proper CA verification, etc.

--- certainty.c
+++ certainty.c
@@ -6,7 +6,31 @@
 #include <openssl/x509v3.h>
 #include <stdio.h>
 #include <err.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+const char *program = "certainty";
+char        ca_file[PATH_MAX] = "ca/overnet.pem";
+char        crl_file[PATH_MAX] = "ca/overnet.crl";
+int         overnet_roles_nid;
+X509_STORE *store;
+
+extern char *optarg;
+extern int   optind, opterr, optopt;
+
+/*
+ *  The verification callback can be used to customise the operation of
+ *  certificate verification, for instance by overriding error conditions or
+ *  logging errors for debugging purposes.
+ *
+ *  The ok parameter to the callback indicates the value the callback should
+ *  return to retain the default behaviour. If it is zero then an error
+ *  condition is indicated. If it is 1 then no error occurred. If the flag
+ *  X509_V_FLAG_NOTIFY_POLICY is set then ok is set to 2 to indicate the policy
+ *  checking is complete.
+ */
 int
 verify_callback(int ok, X509_STORE_CTX *ctx)
 {
@@ -20,7 +44,7 @@ verify_callback(int ok, X509_STORE_CTX *ctx)
 }
 
 int
-decode_crap(X509_EXTENSION *ext)
+decode_overnet_roles(X509_EXTENSION *ext)
 {
 	ASN1_OCTET_STRING *asn1str;
 	unsigned char *data, *p;
@@ -71,38 +95,37 @@ valid_time(X509 *crt)
 	return 0;
 }
 
-int
-main()
+void
+usage()
 {
-	X509_STORE             *store;
-	X509_LOOKUP            *lookup;
-	X509                   *crt;
-	X509_EXTENSION         *ex;
-	FILE                   *f;
-	int                     overnet_roles_nid;
-	int                     roles_idx;
-	X509_NAME              *subject;
-	char                    common_name[256];
+	printf("Usage: %s [options] <command>\n", program);
+	printf("    -h                      Prints this help\n");
+	printf("    -C <ca file>            Specify an alternate Certificate Authority\n");
+	printf("\n");
+	printf("  Commands:\n");
+	printf("    verify <certificate>    Ensures the certificate is signed by "
+	    "our\n");
+	printf("                            authority\n");
+}
 
-	crt = X509_new();
+int
+verify(const char *cert_path)
+{
+	int             roles_idx;
+	X509           *crt;
+	X509_EXTENSION *ex;
+	X509_NAME      *subject;
+	char            common_name[256];
+	FILE           *f;
+	int             r;
+	X509_STORE_CTX *ctx;
 
-	if ((store = X509_STORE_new()) == NULL)
-		err(1, "X509_STORE_new");
-	X509_STORE_set_verify_cb_func(store, verify_callback);
-
-	if ((lookup = X509_STORE_add_lookup(store, X509_LOOKUP_file())) == NULL) {
+	if ((crt = X509_new()) == NULL) {
 		ERR_print_errors_fp(stderr);
 		exit(1);
 	}
 
-	if (!X509_load_cert_file(lookup, "ca/overnet.pem", X509_FILETYPE_PEM)) {
-		ERR_print_errors_fp(stderr);
-		exit(1);
-	}
-
-	X509_LOOKUP_free(lookup);
-
-	if ((f = fopen("ca/overnet.pem", "r")) == NULL)
+	if ((f = fopen(cert_path, "r")) == NULL)
 		err(1, "fopen");
 	if ((crt = PEM_read_X509(f, NULL, NULL, NULL)) == NULL) {
 		ERR_print_errors_fp(stderr);
@@ -110,18 +133,6 @@ main()
 	}
 	fclose(f);
 
-	overnet_roles_nid = OBJ_create("1.3.6.1.4.1.35910.3.1",
-	    "overnetRoles", "Overnet Security Roles");
-	if (overnet_roles_nid == NID_undef)
-		err(1, "OBJ_create");
-
-	roles_idx = X509_get_ext_by_NID(crt, overnet_roles_nid, -1);
-	if (roles_idx == -1)
-		errx(1, "overnetRoles extension not found");
-	if ((ex = X509_get_ext(crt, roles_idx)) == NULL) {
-		ERR_print_errors_fp(stderr);
-		exit(1);
-	}
 	subject = X509_get_subject_name(crt);
 	if (subject == NULL) {
 		ERR_print_errors_fp(stderr);
@@ -134,10 +145,102 @@ main()
 		exit(1);
 	}
 
-	printf("name: %s\n", common_name);
-	if (!valid_time(crt)) {
+	printf("Common name: %s\n", common_name);
+	if (valid_time(crt) == -1) {
 		errx(1, "cert is not valid");
 	}
 
-	return decode_crap(ex);
+	if ((ctx = X509_STORE_CTX_new()) == NULL) {
+		ERR_print_errors_fp(stderr);
+		exit(1);
+	}
+	if (!X509_STORE_CTX_init(ctx, store, crt, NULL)) {
+		ERR_print_errors_fp(stderr);
+		exit(1);
+	}
+
+	if ((r = X509_verify_cert(ctx)) < 0) {
+		ERR_print_errors_fp(stderr);
+		errx(1, "X509_verify_cert error");
+	} else if (r == 0) {
+		ERR_print_errors_fp(stderr);
+		errx(1, "X509_verify_cert failed");
+	}
+
+	roles_idx = X509_get_ext_by_NID(crt, overnet_roles_nid, -1);
+	if (roles_idx == -1)
+		errx(1, "overnetRoles extension not found");
+	if ((ex = X509_get_ext(crt, roles_idx)) == NULL) {
+		ERR_print_errors_fp(stderr);
+		exit(1);
+	}
+
+	return decode_overnet_roles(ex);
+}
+
+int
+main(int argc, char **argv)
+{
+	int          opt;
+	char        *command;
+	X509_LOOKUP *lookup;
+
+	while ((opt = getopt(argc, argv, "hC:")) != -1) {
+		switch (opt) {
+		case 'h':
+			usage();
+			exit(0);
+		case 'C':
+			strlcpy(ca_file, optarg, sizeof(ca_file));
+			break;
+		}
+	}
+
+	if (optind >= argc) {
+		usage();
+		exit(1);
+	}
+
+	overnet_roles_nid = OBJ_create("1.3.6.1.4.1.35910.3.1",
+	    "overnetRoles", "Overnet Security Roles");
+	if (overnet_roles_nid == NID_undef)
+		err(1, "OBJ_create");
+
+	if ((store = X509_STORE_new()) == NULL)
+		err(1, "X509_STORE_new");
+	X509_STORE_set_verify_cb_func(store, verify_callback);
+	if ((lookup = X509_STORE_add_lookup(store, X509_LOOKUP_file())) == NULL) {
+		ERR_print_errors_fp(stderr);
+		exit(1);
+	}
+	if (!X509_load_cert_file(lookup, ca_file, X509_FILETYPE_PEM)) {
+		ERR_print_errors_fp(stderr);
+		exit(1);
+	}
+	if (!X509_load_crl_file(lookup, crl_file, X509_FILETYPE_PEM)) {
+		ERR_print_errors_fp(stderr);
+		exit(1);
+	}
+	if (!X509_STORE_set_flags(store, X509_V_FLAG_X509_STRICT|
+	    X509_V_FLAG_CRL_CHECK|X509_V_FLAG_CRL_CHECK_ALL)) {
+		ERR_print_errors_fp(stderr);
+		exit(1);
+	}
+	/*
+	 * We're not calling X509_LOOKUP_free() as this causes a segfault
+	 * if we try reusing X509_LOOKUP_file().
+	 */
+
+	command = argv[optind++];
+
+	if (strcmp(command, "verify") == 0) {
+		if (optind >= argc) {
+			errx(1, "no certificate file provided");
+			exit(1);
+		}
+		return verify(argv[optind++]);
+	}
+
+	usage();
+	return 1;
 }
