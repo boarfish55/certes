6025c7c6ac45e95b49cad83a52c8339b0d5cb6f2
Pascal Lalonde <plalonde@overnet.ca>
2024-06-08 17:38:36 -0400

    Updated mdr, added a few utility functions

--- util.c
+++ util.c
@@ -115,3 +115,162 @@ drop_privileges(const char *user, const char *group, struct xerr *e)
 		return XERRF(e, XLOG_ERRNO, errno, "seteuid");
 	return 0;
 }
+
+#define CLOSE_X(fd) close_x(fd, #fd, __func__, __LINE__)
+void
+close_x(int fd, const char *fd_name, const char *fn, int line)
+{
+	if (close(fd) == -1)
+		xlog_strerror(LOG_ERR, errno, "%s:%d: close(%s)",
+		    fn, line, fd_name);
+}
+
+ssize_t
+writeall(int fd, const void *buf, size_t count)
+{
+	ssize_t w;
+	ssize_t n = 0;
+
+	while (n < count) {
+		w = write(fd, buf + n, count - n);
+		if (w == -1) {
+			if (errno == EINTR)
+				continue;
+			return -1;
+		}
+		n += w;
+	}
+	return n;
+}
+
+ssize_t
+readall(int fd, void *buf, size_t count)
+{
+	ssize_t r;
+	ssize_t n = 0;
+
+	while (n < count) {
+		r = read(fd, buf + n, count - n);
+		if (r == -1) {
+			if (errno == EINTR)
+				continue;
+			return -1;
+		} else if (r == 0) {
+			return n;
+		}
+		n += r;
+	}
+	return n;
+}
+
+int
+spawn(char *const argv[], int *stdin, int *stdout, struct xerr *e)
+{
+	pid_t pid;
+	int   p_in[2];
+	int   p_out[2];
+	int   null_fd;
+
+	if (pipe(p_in) == -1)
+		return XERRF(e, XLOG_ERRNO, errno, "pipe");
+	*stdin = p_in[1];
+
+	if (pipe(p_out) == -1) {
+		XERRF(e, XLOG_ERRNO, errno, "pipe");
+		CLOSE_X(p_in[0]);
+		CLOSE_X(p_in[1]);
+		return -1;
+	}
+	*stdout = p_out[0];
+
+	if ((pid = fork()) == -1) {
+		XERRF(e, XLOG_ERRNO, errno, "fork");
+		CLOSE_X(p_in[0]);
+		CLOSE_X(p_in[1]);
+		CLOSE_X(p_out[0]);
+		CLOSE_X(p_out[1]);
+		return -1;
+	} else if (pid == 0) {
+		CLOSE_X(p_in[1]);
+		CLOSE_X(p_out[0]);
+		if (p_in[0] != STDIN_FILENO) {
+			if (dup2(p_in[0], STDIN_FILENO) == -1) {
+				XERRF(e, XLOG_ERRNO, errno, "dup2");
+				_exit(1);
+			}
+			CLOSE_X(p_in[0]);
+		}
+		if (p_out[1] != STDOUT_FILENO) {
+			if (dup2(p_out[1], STDOUT_FILENO) == -1) {
+				XERRF(e, XLOG_ERRNO, errno, "dup2");
+				_exit(1);
+			}
+			CLOSE_X(p_out[1]);
+		}
+
+		if ((null_fd = open("/dev/null", O_RDWR)) == -1) {
+			xlog_strerror(LOG_ERR, errno, "open /dev/null");
+			exit(1);
+		}
+		dup2(null_fd, STDERR_FILENO);
+		if (null_fd > 2)
+			close(null_fd);
+
+		if (chdir("/") == -1) {
+			XERRF(e, XLOG_ERRNO, errno, "chdir");
+			_exit(1);
+		}
+
+		if (execv(argv[0], argv) == -1) {
+			XERRF(e, XLOG_ERRNO, errno, "execv: %s", argv[0]);
+			_exit(1);
+		}
+	}
+
+	CLOSE_X(p_in[0]);
+	CLOSE_X(p_out[1]);
+
+	return 0;
+}
+
+char **
+cmdargv(char *command)
+{
+	char **argv;
+	char  *p;
+	int    in_arg;
+	int    n = 0, i;
+
+	for (in_arg = 0, p = command; *p != '\0'; p++) {
+		if (!in_arg && *p != ' ') {
+			n++;
+			in_arg = 1;
+			continue;
+		}
+
+		if (*p == ' ') {
+			in_arg = 0;
+			continue;
+		}
+	}
+
+	argv = malloc(sizeof(char *) * (n + 1));
+	if (argv == NULL)
+		return NULL;
+	argv[n] = NULL;
+
+	for (in_arg = 0, i = 0, p = command; i < n; p++) {
+		if (!in_arg && *p != ' ') {
+			argv[i++] = p;
+			in_arg = 1;
+			continue;
+		}
+
+		if (*p == ' ') {
+			in_arg = 0;
+			*p = '\0';
+			continue;
+		}
+	}
+	return argv;
+}
--- util.h
+++ util.h
@@ -7,4 +7,12 @@ int is_hex_str(const char *);
 int daemonize(const char *, const char *, int, int, struct xerr *);
 int drop_privileges(const char *, const char *, struct xerr *);
 
+ssize_t writeall(int, const void *, size_t);
+ssize_t readall(int, void *, size_t);
+
+#define CLOSE_X(fd) close_x(fd, #fd, __func__, __LINE__)
+void   close_x(int, const char *, const char *, int);
+int    spawn(char *const argv[], int *, int *, struct xerr *);
+char **cmdargv(char *);
+
 #endif
