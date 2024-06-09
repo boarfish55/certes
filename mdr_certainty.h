75f00cdec426f5c8e8f3a4c5789f9140cae231ec
Pascal Lalonde <plalonde@overnet.ca>
2024-06-09 18:26:40 -0400

    Add backend message def

--- mdr_certainty.h
+++ mdr_certainty.h
@@ -6,5 +6,8 @@
 #define MDR_NS_CERTAINTY 0x00000002
 
 #define MDR_ID_CERTAINTY_BOOTSTRAP 0x0001
+#define MDR_ID_CERTAINTY_BEMSG     0x0002
+
+int pack_bemsg(struct mdr *, uint64_t, int, struct mdr *, X509 *);
 
 #endif
