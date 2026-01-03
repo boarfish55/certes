bfd56e9854dbfff853b7e934072622065e81a1e1
Pascal Lalonde <plalonde@overnet.ca>
2026-01-03 11:35:41 -0500

    Move all test data un testdata/

--- bootstrap_ca.sh
+++ bootstrap_ca.sh
@@ -5,94 +5,95 @@ set -e
 DOMAIN=example.com
 ORG=Example
 
-rm -rf ca
-mkdir -p ca/certs
-echo "01" > ca/serial
-touch ca/index.txt
+basedir=testdata
+
+rm -rf $basedir
+
+mkdir -p $basedir/ca/certs
+echo "01" > $basedir/ca/serial
+touch $basedir/ca/index.txt
 
 # Create self-signed root certificate; in a real situation, the key should
 # be kept on a secure machine or even offline storage. The certificate and
 # CRL will need to be deployed on all agents in the fleet.
 openssl req -x509 -nodes -config certalator.cnf -section root_ca \
-	-newkey ed25519 -keyout ca/key.pem \
-	-out ca/root.pem -outform PEM -days 365 \
+	-newkey ed25519 -keyout $basedir/ca/key.pem \
+	-out $basedir/ca/root.pem -outform PEM -days 365 \
 	-extensions root_ext \
 	-subj "/emailAddress=cert@$DOMAIN/O=$ORG/CN=$ORG CA"
 
 # Each "authority" can create and renew certs. They are intermediate
 # signing authorities.
-rm -rf authority1
-mkdir -p authority1/certs authority1/trust_store
-echo "01" > authority1/serial
-touch authority1/index.txt
+mkdir -p $basedir/authority1/certs $basedir/authority1/trust_store
+echo "01" > $basedir/authority1/serial
+touch $basedir/authority1/index.txt
 openssl req -nodes -config certalator.cnf -newkey ed25519 \
-	-keyout authority1/key.pem -keyform PEM \
-	-out authority1/req.pem -outform PEM \
+	-keyout $basedir/authority1/key.pem -keyform PEM \
+	-out $basedir/authority1/req.pem -outform PEM \
 	-subj "/O=$ORG/CN=authority1.$DOMAIN" \
 	-addext "subjectAltName = DNS:authority1.$DOMAIN"
 # Sign authority1 cert & verify
 yes | openssl ca -config certalator.cnf -name root_ca \
-	-in authority1/req.pem -extensions intermediate_ca_ext \
-	-out authority1/cert.pem
-openssl verify -CAfile ca/root.pem authority1/cert.pem
-cp ca/root.pem authority1/trust_store/
-cp authority1/cert.pem authority1/trust_store/
-openssl rehash authority1/trust_store
+	-in $basedir/authority1/req.pem -extensions intermediate_ca_ext \
+	-out $basedir/authority1/cert.pem
+openssl verify -CAfile $basedir/ca/root.pem $basedir/authority1/cert.pem
+cp $basedir/ca/root.pem $basedir/authority1/trust_store/
+cp $basedir/authority1/cert.pem $basedir/authority1/trust_store/
+openssl rehash $basedir/authority1/trust_store
 # Generate CRL
 openssl ca -config certalator.cnf \
-	-gencrl -out authority1/authority1.crl
+	-gencrl -out $basedir/authority1/authority1.crl
 
 # Create a "ca-proxy" cert request for an mdrd daemon
-rm -rf proxy1
-mkdir -p proxy1
+mkdir -p $basedir/proxy1
 openssl req -nodes -config certalator.cnf -newkey ed25519 \
-	-keyout proxy1/key.pem -keyform PEM \
-	-out proxy1/req.pem -outform PEM \
+	-keyout $basedir/proxy1/key.pem -keyform PEM \
+	-out $basedir/proxy1/req.pem -outform PEM \
 	-subj "/O=$ORG/CN=proxy1.$DOMAIN" \
 	-addext "subjectAltName=DNS:proxy1.$DOMAIN,IP:172.16.5.14,IP:fe80::c2a5:e8ff:fe29:5874"
 # Sign proxy1 cert & verify
-yes | openssl ca -config certalator.cnf -in proxy1/req.pem \
-	-out proxy1/cert.pem -extensions intermediate_proxy_crt_ext
-openssl verify -CApath authority1/trust_store proxy1/cert.pem
-cat ca/root.pem authority1/cert.pem > proxy1/ca.pem
+yes | openssl ca -config certalator.cnf -in $basedir/proxy1/req.pem \
+	-out $basedir/proxy1/cert.pem -extensions intermediate_proxy_crt_ext
+openssl verify -CApath $basedir/authority1/trust_store $basedir/proxy1/cert.pem
+cat $basedir/ca/root.pem $basedir/authority1/cert.pem > $basedir/proxy1/ca.pem
 
 # Create a "client1" cert request
-rm -rf client1
-mkdir -p client1
+mkdir -p $basedir/client1
 openssl req -nodes -config certalator.cnf -newkey ed25519 \
-	-keyout client1/key.pem -keyform PEM \
-	-out client1/req.pem -outform PEM \
+	-keyout $basedir/client1/key.pem -keyform PEM \
+	-out $basedir/client1/req.pem -outform PEM \
 	-subj "/O=$ORG/CN=client1.$DOMAIN" \
 	-addext "subjectAltName=DNS:client1.$DOMAIN,IP:172.16.5.14,IP:fe80::c2a5:e8ff:fe29:5874"
 # Sign client1 cert & verify
-yes | openssl ca -config certalator.cnf -in client1/req.pem \
-	-out client1/cert.pem -extensions intermediate_client_crt_ext
-openssl verify -CApath authority1/trust_store client1/cert.pem
+yes | openssl ca -config certalator.cnf -in $basedir/client1/req.pem \
+	-out $basedir/client1/cert.pem -extensions intermediate_client_crt_ext
+openssl verify -CApath $basedir/authority1/trust_store $basedir/client1/cert.pem
 
 # Create a "client2" cert request
-rm -rf client2
-mkdir -p client2
+mkdir -p $basedir/client2
 openssl req -nodes -config certalator.cnf -newkey ed25519 \
-	-keyout client2/key.pem -keyform PEM \
-	-out client2/req.pem -outform PEM \
+	-keyout $basedir/client2/key.pem -keyform PEM \
+	-out $basedir/client2/req.pem -outform PEM \
 	-subj "/O=$ORG/CN=client2.$DOMAIN" \
 	-addext "subjectAltName = DNS:client2.$DOMAIN"
 
 # Sign and revoke client2 cert
-yes | openssl ca -config certalator.cnf -in client2/req.pem \
-	-out client2/cert.pem -extensions intermediate_client_crt_ext
-openssl verify -CApath authority1/trust_store client2/cert.pem
-openssl ca -config certalator.cnf -revoke client2/cert.pem
+yes | openssl ca -config certalator.cnf -in $basedir/client2/req.pem \
+	-out $basedir/client2/cert.pem -extensions intermediate_client_crt_ext
+openssl verify -CApath $basedir/authority1/trust_store $basedir/client2/cert.pem
+openssl ca -config certalator.cnf -revoke $basedir/client2/cert.pem
 
 # Generate CRL
 openssl ca -config certalator.cnf -section root_ca \
-	-gencrl -out ca/root.crl
+	-gencrl -out $basedir/ca/root.crl
 # View it and verify signature
-openssl crl -in ca/root.crl -text -noout -CAfile ca/root.pem
-cat ca/root.crl authority1/authority1.crl > proxy1/ca.crl
+openssl crl -in $basedir/ca/root.crl -text -noout -CAfile $basedir/ca/root.pem
+cat $basedir/ca/root.crl \
+	$basedir/authority1/authority1.crl > $basedir/proxy1/ca.crl
 
 # Test and see if client2's cert is indeed revoked, as it should
-openssl verify -CApath authority1/trust_store -CRLfile ca/root.crl \
-	-crl_check client2/cert.pem || true
+openssl verify -CApath $basedir/authority1/trust_store \
+	-CRLfile $basedir/ca/root.crl \
+	-crl_check $basedir/client2/cert.pem || true
 
 echo "All good!"
